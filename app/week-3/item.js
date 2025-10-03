49d81a070d456d80092bbf276d5102d8a433fbf2
Ambraxil <potatololhi@gmail.com>
2025-10-03 16:09:39 -0600

    gagaga

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Item = ({ name, quantity, category }) => {
   return (
     <li className="p-4 mb-4 w-100 rounded flex justify-between items-center mx-auto bg-blue-950">
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import ItemList from './items-list';
 
 const Page = () => {
