420f67be1e3b7ef303b67b0fff57d9d1d1e5f80e
Ambraxil <potatololhi@gmail.com>
2025-10-03 16:05:47 -0600

    git

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Item = ({ name, quantity, category }) => {
   return (
-    <li className="p-4 mb-4 w-100 rounded shadow-sm flex justify-between items-center mx-auto bg-blue-950">
+    <li className="p-4 mb-4 w-100 rounded flex justify-between items-center mx-auto bg-blue-950">
       <div>
         <p className="font-bold text-xl">{name}</p>
         <p className="font-semibold text-sm text-white-600">Buy {quantity} in {category}</p>
