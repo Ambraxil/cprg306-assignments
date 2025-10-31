5b43dfbcb31379331a2e535b07338339083178f6
Ambraxil <potatololhi@gmail.com>
2025-10-31 05:49:56 -0600

    gagaga

--- app/page.js
+++ app/page.js
@@ -17,6 +17,9 @@ export default function Home() {
       <Link href="/week-6">Week 6</Link>
       <br/>
       <Link href="/week-7">Week 7</Link>
+      <br/>
+      <Link href="/week-8">Week 8</Link>
+      <br/>
     </main> 
   );
 }
